939391752358d1caf7691f94399d512e1d5484b4
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-16 13:16:58 -0200

    Visualização e Edição de Categoria Na edição utilizamos o FormBuilder para construir o formulário

--- app/Forms/CategoryForm.php
+++ app/Forms/CategoryForm.php
@@ -8,7 +8,11 @@ class CategoryForm extends Form
 {
     public function buildForm()
     {
+        $id = $this->getData('id');
         $this
-            ->add('category', 'word');
+            ->add('category', 'text', [
+                'label' => 'Categoria',
+                'rules' => "required|max:255|unique:categories, $id"
+            ]);
     }
 }
