a7ebd14dfece69f8fcadb974a8383f994c759c47
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-16 17:00:45 -0200

    Implementação do Formulário de alteração de senha

--- app/Forms/UserSettingsForm.php
+++ app/Forms/UserSettingsForm.php
@@ -9,7 +9,10 @@ class UserSettingsForm extends Form
     public function buildForm()
     {
         $this
-            ->add('password', 'password')
+            ->add('password', 'password',[
+                'rules' => 'required|min:6|max:16|confirmed'
+                //Confirmed: este password precisa ser confirmado para o formulário ser processado
+            ])
             ->add('password_confirmation', 'password');
     }
 }
