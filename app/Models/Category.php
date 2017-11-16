bfef15c50ef41737bd9635033711149a72dfe77c
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-16 11:56:52 -0200

    Implementação da TableInterface com os métodos para exibição dos títulos e valores das colunas

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,16 +2,45 @@
 
 namespace CodeFlix\Models;
 
+use Bootstrapper\Interfaces\TableInterface;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class Category extends Model implements Transformable
+class Category extends Model implements TableInterface
 {
     use TransformableTrait;
 
     protected $fillable = [
         'category'
     ];
+
+
+    /**
+     * A list of headers to be used when a table is displayed
+     *
+     * @return array
+     */
+    public function getTableHeaders()
+    {
+        // TODO: Implement getTableHeaders() method.
+        return ['#', 'Categoria', ];
+    }
+
+    /**
+     * Get the value for a given header. Note that this will be the value
+     * passed to any callback functions that are being used.
+     *
+     * @param string $header
+     * @return mixed
+     */
+    public function getValueForHeader($header)
+    {
+        // TODO: Implement getValueForHeader() method.
+        switch ($header){
+            case '#': return $this->id;
+            case 'Categoria': return $this->category;
+        }
+    }
 
 }
