e0482d6859859db7bcfb5378face8f43b92b6613
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-16 17:10:21 -0200

    View de Settings

--- resources/views/admin/auth/setting.blade.php
+++ resources/views/admin/auth/setting.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
     <div class="container">
-    <div class="row">
-        <h3>Editar Usuário</h3>
-        <?php $icon = Icon::create('pencil');?>
-        {!! form($form->add('salve', 'submit',[
-            'attr' => ['class' => 'btn btn-primary btn-block'],
-            'label' => $icon
-            ]))
-        !!}
-    </div>
-
+        <div class="row">
+            <h3>Editar meu perfil</h3>
+            <?php $icon = Icon::create('floppy-disk');?>
+            {!!
+                form($form->add('salvar', 'submit',
+                    [
+                        'attr' => ['class' => 'btn btn-primary btn-block'],
+                        'label' => $icon
+                    ]
+                ))
+            !!}
+        </div>
     </div>
 @endsection
