7e3ee9960d4fe273c4c623066671375d85056565
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-16 11:57:47 -0200

    Factory de Category

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,9 @@
 use CodeFlix\Models\Category;
 use Faker\Generator as Faker;
 
+//Factory de Category
 $factory->define(Category::class, function (Faker $faker) {
-    return ['category' => $faker->unique()->title];
+    return [
+        'category' => $faker->unique()->word
+    ];
 });
