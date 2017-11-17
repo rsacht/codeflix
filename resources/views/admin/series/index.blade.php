bc0c667ceb88880e8aaedc187585bf455a08fb20
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-17 21:22:54 -0200

    Implementação da View de listagem de Séries com  push para o layout para a diminuição da coluna Descrição nth-child(3): terceira coluna

--- resources/views/admin/series/index.blade.php
+++ resources/views/admin/series/index.blade.php
@@ -3,20 +3,28 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>Listagem de Categorias</h3>
-            {!! Button::primary('Nova Categoria')->asLinkTo(route('admin.categories.create')) !!}
+            <h3>Lista de Séries</h3>
+            {!! Button::primary('Nova Série')->asLinkTo(route('admin.series.create')) !!}
         </div>
     <div class="row">
     {!!
-        Table::withContents($categories->items())->striped()->callback('Ações', function($field, $category){
-            $linkEdit = route('admin.categories.edit', [ 'category' => $category->id]);
-            $linkShow = route('admin.categories.show', [ 'category' => $category->id]);
+        Table::withContents($series->items())->striped()->callback('Ações', function($field, $serie){
+            $linkEdit = route('admin.series.edit', [ 'serie' => $serie->id]);
+            $linkShow = route('admin.series.show', [ 'serie' => $serie->id]);
             return Button::link(Icon::create('pencil'))->asLinkTo($linkEdit).'|'.
                    Button::link(Icon::create('remove'))->asLinkTo($linkShow);
         })
     !!}
     </div>
 
-    {!! $categories->links() !!}
+    {!! $series->links() !!}
     </div>
 @endsection
+
+@push('styles')
+    <style type="text/css">
+        table>thead>tr>th:nth-child(3){
+            width:50%;
+        }
+    </style>
+@endpush
