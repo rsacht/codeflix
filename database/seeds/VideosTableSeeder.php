da7f41a8dd76c9a39eb1e96dbddd8895e73b2ec5
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-18 01:15:06 -0200

    Criando um factory para os Vídeos

--- database/seeds/VideosTableSeeder.php
+++ database/seeds/VideosTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 
 class VideosTableSeeder extends Seeder
@@ -11,6 +12,19 @@ class VideosTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        /** @var Collection $series */
+        $series = \CodeFlix\Models\Serie::all();
+        factory(\CodeFlix\Models\Video::class, 100)
+            ->create()
+            ->each(function($video)use($series){
+                $num = rand(1,3);
+                if($num%2==0){
+                    //Série com Vídeo
+                    $serie = $series->random();
+                    $video->serie_id = $serie->id;
+                    $video->serie()->associate($serie);
+                    $video->save();
+                }
+            });
     }
 }
