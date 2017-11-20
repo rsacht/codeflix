d5f51fab2bc3dac2eb35ec768b8d8af07fa61e67
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2017-11-20 16:14:40 -0200

    Implementação da TableInterface no Model Video

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,11 +2,12 @@
 
 namespace CodeFlix\Models;
 
+use Bootstrapper\Interfaces\TableInterface;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class Video extends Model implements Transformable
+class Video extends Model implements Transformable, TableInterface
 {
     use TransformableTrait;
 
@@ -20,4 +21,29 @@ class Video extends Model implements Transformable
         return $this->belongsToMany(Category::class);
     }
 
+    /**
+     * A list of headers to be used when a table is displayed
+     *
+     * @return array
+     */
+    public function getTableHeaders()
+    {
+        return['#'];
+    }
+
+    /**
+     * Get the value for a given header. Note that this will be the value
+     * passed to any callback functions that are being used.
+     *
+     * @param string $header
+     * @return mixed
+     */
+    public function getValueForHeader($header)
+    {
+        switch ($header){
+            case '#':
+                return $this->id;
+                break;
+        }
+    }
 }
