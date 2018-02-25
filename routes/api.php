020d3d9e0232a53e1c0d25c0d7b61f810f7874eb
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-02-25 07:14:03 -0300

    DINGO API - Criando rota para teste no Postman

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+\ApiRoute::get('/test', function(){
+    return \CodeFlix\Models\User::paginate();
+});
