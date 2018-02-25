8fa2521757ecdc0a6df9b7e758cb0f1c972d2dc3
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-02-25 07:29:47 -0300

    DINGO API - O teste com a rota não funcionou porque a Dingo Api exige a utilização de versões para as rotas

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-\ApiRoute::get('/test', function(){
-    return \CodeFlix\Models\User::paginate();
+\ApiRoute::version('v1', function(){
+    \ApiRoute::get('/test', function(){
+        return \CodeFlix\Models\User::paginate();
+    });
 });
