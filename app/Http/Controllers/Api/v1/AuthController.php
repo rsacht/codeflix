1368fcb4888e5059b1b10279705f6defa0fab013
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-02-25 07:55:54 -0300

    Método para gerar o token em formato JSON

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -4,8 +4,13 @@ namespace CodeFlix\Http\Controllers\Api\v1;
 
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use CodeFlix\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use AuthenticatesUsers;
+
+    protected function sendLoginResponse(Request $request, $token){
+        return ['token' => $token];
+    }
 }
