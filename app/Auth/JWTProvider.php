a4df0f011ba67564619276c08bc7d1a27a16dcd7
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-02-25 09:09:50 -0300

    Adicionando os métodos de implementação obrigatória da interface Authorization

--- app/Auth/JWTProvider.php
+++ app/Auth/JWTProvider.php
@@ -4,8 +4,32 @@ namespace CodeFlix\Auth;
 
 
 use Dingo\Api\Auth\Provider\Authorization;
+use Dingo\Api\Routing\Route;
+use Illuminate\Http\Request;
 
 class JWTProvider extends Authorization
 {
 
+    /**
+     * Get the providers authorization method.
+     *
+     * @return string
+     */
+    public function getAuthorizationMethod()
+    {
+        // TODO: Implement getAuthorizationMethod() method.
+    }
+
+    /**
+     * Authenticate the request and return the authenticated user instance.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Dingo\Api\Routing\Route $route
+     *
+     * @return mixed
+     */
+    public function authenticate(Request $request, Route $route)
+    {
+        // TODO: Implement authenticate() method.
+    }
 }
