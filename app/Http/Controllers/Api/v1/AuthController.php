e0aa9179b33fed2f1ab9aa9ef61e53338e9e4697
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-02-25 07:52:22 -0300

    Criando o controller de autenticação para a API

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CodeFlix\Http\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+use CodeFlix\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    //
+}
